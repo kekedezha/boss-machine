cfc0ef0880bcc6c686d7839e9663e5f4815d70e9
kekedezha <dezhac6@gmail.com>
2024-03-26 12:12:56 -0400

    added endpoints folder for different /api routes. will contain the required routes for project.

---
+++ server/apiPathEndpoints/ideas.js
@@ -0,0 +1,8 @@
+// Import the Express library
+const express = require('express');
+// Create an instance of an Express router and save it under ideasRouter
+const ideasRouter = express.Router();
+
+
+
+module.exports = ideasRouter;
---
+++ server/apiPathEndpoints/minions.js
@@ -0,0 +1,8 @@
+// Import the Express library
+const express = require('express');
+// Create an instance of an Express router and save it under minionsRouter
+const minionsRouter = express.Router();
+
+
+
+module.exports = minionsRouter;

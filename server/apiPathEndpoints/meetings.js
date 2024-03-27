61d2a6e312d46cb85d4f7ecbef27d48864844ac1
kekedezha <dezhac6@gmail.com>
2024-03-27 17:14:33 -0400

    updated post request to use createMeeting method from db.js

--- server/apiPathEndpoints/meetings.js
+++ server/apiPathEndpoints/meetings.js
@@ -1,6 +1,6 @@
 const express = require('express'); // Import the Express library
 const meetingsRouter = express.Router(); // Create an instance of an Express router and save it under meetingsRouter
-const { getAllFromDatabase, deleteAllFromDatabase, addToDatabase } = require('../db') // import database functions from './db.js'
+const { getAllFromDatabase, deleteAllFromDatabase, createMeeting } = require('../db') // import database functions from './db.js'
 
 // GET request that returns an array of all meetings
 meetingsRouter.get('/', (req,res,next) => {
@@ -9,14 +9,7 @@ meetingsRouter.get('/', (req,res,next) => {
 
 // POST request that creates a new meeting and saves it to the database
 meetingsRouter.post('/', (req,res,next) => {
-    const newMeetingToCreate = req.body;
-
-    if (newMeetingToCreate.time && newMeetingToCreate.date && newMeetingToCreate.day && newMeetingToCreate.note) {
-        const newIdea = addToDatabase('meetings', newMeetingToCreate);
-        res.status(201).send(newIdea);
-    } else {
-        res.status(400).send("Meetings must have all fields filled out to be created!")
-    }
+    res.status(201).send(createMeeting());
 })
 
 // DELETE request that deletes ALL meetings from the database

22c7c0d65b5e75924d98d372b1a37619bab51b17
kekedezha <dezhac6@gmail.com>
2024-03-28 15:24:04 -0400

    updated meetings.js to pass all tests scripts.

--- server/apiPathEndpoints/meetings.js
+++ server/apiPathEndpoints/meetings.js
@@ -1,6 +1,6 @@
 const express = require('express'); // Import the Express library
 const meetingsRouter = express.Router(); // Create an instance of an Express router and save it under meetingsRouter
-const { getAllFromDatabase, deleteAllFromDatabase, createMeeting } = require('../db') // import database functions from './db.js'
+const { getAllFromDatabase, deleteAllFromDatabase, createMeeting, addToDatabase } = require('../db') // import database functions from './db.js'
 
 // GET request that returns an array of all meetings
 meetingsRouter.get('/', (req,res,next) => {
@@ -9,11 +9,11 @@ meetingsRouter.get('/', (req,res,next) => {
 
 // POST request that creates a new meeting and saves it to the database
 meetingsRouter.post('/', (req,res,next) => {
-    res.status(201).send(createMeeting());
+    res.status(201).send(addToDatabase('meetings', createMeeting()));
 })
 
 // DELETE request that deletes ALL meetings from the database
 meetingsRouter.delete('/', (req,res,next) => {
-    res.status(200).send(deleteAllFromDatabase('meetings'));
+    res.status(204).send(deleteAllFromDatabase('meetings'));
 })
 module.exports = meetingsRouter;
